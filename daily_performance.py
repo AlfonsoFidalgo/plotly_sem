9c75fefc526caba5f839e305d598c820851280ac
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2018-11-15 12:14:01 +0100

    added moving avg graphs

--- daily_performance.py
+++ daily_performance.py
@@ -14,7 +14,7 @@ conn = pc.connect(database=db.credentials['db_name'],
                                port=db.credentials['db_port'])
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM heycar_report.marketing_sea_performance_daily WHERE day > current_date - 50")
+cur.execute("SELECT * FROM heycar_report.marketing_sea_performance_daily WHERE day > current_date - 80")
 conn.commit()
 data = cur.fetchall()
 df = pd.DataFrame(data)
@@ -27,13 +27,35 @@ df.columns = ['day', 'source', 'account_id', 'account_name',
 daily = df[['day', 'cost', 'leads']].groupby(['day']).sum().reset_index()
 daily['cpa'] = daily['cost'] / daily['leads']
 
+#MOVING AVERAGES FROM LEADS AND CPA
+daily['cost_7'] = daily.rolling(window=7)['cost'].sum()
+daily['leads_7'] = daily.rolling(window=7)['leads'].sum()
+daily['cpa_7'] = daily['cost_7'] / daily['leads_7']
+daily['leads_7_avg'] = daily['leads_7'] / 7
+
 #TRACES
 cpa = go.Scatter(
     x=daily['day'],
     y=daily['cpa'],
     name='cpa',
     mode='lines',
+    opacity=0.4,
     line={'width': 2, 'smoothing': 1, 'shape': 'spline'}
+)
+
+cpa_7 = go.Scatter(
+    x=daily['day'],
+    y=daily['cpa_7'],
+    name='7 days avg. CPA',
+    line={'width': 3}
+)
+
+leads_7 = go.Scatter(
+    x=daily['day'],
+    y=daily['leads_7_avg'],
+    name='7 days avg. Leads',
+    yaxis='y2',
+    line={'width': 3}
 )
 
 leads = go.Scatter(
@@ -42,11 +64,12 @@ leads = go.Scatter(
     name='leads',
     mode='lines',
     yaxis='y2',
+    opacity=0.4,
     line={'width': 2, 'smoothing': 1, 'shape': 'spline'}
-    # opacity=0.5
+
 )
 
-data = [cpa, leads]
+data = [cpa, cpa_7, leads, leads_7]
 layout = go.Layout(
     title='Daily CPA & Lead volume',
     yaxis=dict(title='CPA'),
